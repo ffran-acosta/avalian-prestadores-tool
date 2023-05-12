f44bf74a66103e77c0d187b5800514b8fc2ae26e
Franco Acosta <francoaugustoacosta@gmail.com>
2023-05-11 22:35:01 -0300

    commit and start auth controller

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -7,24 +7,3 @@ router.get('/', (req, res) => {
 
 export default router;
 
-
-// router.get('/', (req, res) => {
-//     res.send('Get all users');
-// });
-
-// router.post('/', (req, res) => {
-//     res.send('Create a user');
-// });
-
-// router.get('/:id', (req, res) => {
-//     res.send(`Get user ${req.params.id}`);
-// });
-
-// router.put('/:id', (req, res) => {
-//     res.send(`Update user ${req.params.id}`);
-// });
-
-// router.delete('/:id', (req, res) => {
-//     res.send(`Delete user ${req.params.id}`);
-// });
-
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,12 +3,9 @@ const router = express.Router();
 
 import { controller } from '../controller';
 
-
 router.get('/users', controller.getUsers);
 router.get('/users/:id', controller.getUserById);
 router.post('/create', controller.createUser);
 router.delete('/:id', controller.deleteUser);
-
-
 
 export default router;
